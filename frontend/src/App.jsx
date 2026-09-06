21cabe6d8be019ed56b63f2632a97683cdca1272
P N Megarajan <megarajan2006@gmail.com>
2026-09-06 11:38:05 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Doctors from './pages/Doctors'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/myprofile' element={<MyProfile />} />
+        <Route path='/myappointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
